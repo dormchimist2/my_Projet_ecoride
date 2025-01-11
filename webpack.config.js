edd31ee6dbfab3a2624474e947e41d50744c8ff4
polonium420 <polonium420@hotmail.com>
2025-01-11 08:39:08 +0100

    test style css de boostrap

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ Encore
 
     // enables Sass/SCSS support
     .enableSassLoader()
+    .enableStimulusBridge('./assets/controllers.json');
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
