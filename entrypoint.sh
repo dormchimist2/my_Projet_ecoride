e96c7f8d4e9ea0e4b8a485bde5e25c4b42a2c40a
TheLastNox <32538133+TheLastNox@users.noreply.github.com>
2025-02-19 17:22:16 +0000

    mend

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# Écriture de la variable DATABASE_URL dans .env.dev
-# echo "DATABASE_URL=postgresql://ecowhat_user:KsYwBbfZNOV57vwRCgmUOYI26aUN2PfH@dpg-cuqs8n56l47c73chtkn0-a.frankfurt-postgres.render.com/ecowhat" > /var/www/symfony/.env.dev
-
-# cat "DATABASE_URL=${DATABASE_URL}" > /var/www/symfony/.env.local
-cat /var/www/symfony/.env.dev
-
+# Écriture de la variable DATABASE_URL dans .env.local
 echo "DATABASE_URL=${DATABASE_URL}" > /var/www/symfony/.env.local
 
 # Lancer les migrations (optionnel, si nécessaire)
-# php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:migrations:migrate --no-interaction
 
 # Démarrer PHP-FPM et Nginx
 php-fpm82 & nginx -g 'daemon off;'
