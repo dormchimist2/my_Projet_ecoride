262c914cc9e21e09bb0e8281076223e32fd8c596
polonium420 <polonium420@hotmail.com>
2025-03-02 07:56:31 +0100

    mend

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Générer les migrations si nécessaire
+php bin/console doctrine:migrations:diff --no-interaction
+
 # Exécuter les migrations
 php bin/console doctrine:migrations:migrate --no-interaction
